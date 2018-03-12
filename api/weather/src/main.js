3362268f4747a3003c9049c89f1844591228168f
Jordan Liekweg <jordanliekweg@ool-ad0203c5.dyn.optonline.net>
2018-03-12 09:43:41 -0400

    Figured out I wasn't able to access my zipCode variable

--- api/weather/src/main.js
+++ api/weather/src/main.js
@@ -4,38 +4,64 @@ let getWeatherAsJSON = url => {
   return fetch(url).then(response => response.json());
 };
 
-let displayWeather = () => {
-  getWeatherAsJSON(
-    "http://api.openweathermap.org/data/2.5/weather?q=New+York&units=imperial&appid=7612edd4a0c941f2e5078f8c82d6e4a3"
-  ).then(json => {
-    let weatherTemp = document.createElement("p");
-    weatherTemp.innerHTML = json.main.temp;
-    let weatherContainer = document.querySelector(".currentTemp");
-    weatherContainer.appendChild(weatherTemp);
-    let cityName = document.createElement("p");
-    cityName.innerHTML = json.name;
-    let nameContainer = document.querySelector(".cityName");
-    nameContainer.appendChild(cityName);
-    let minTemp = document.createElement("p");
-    minTemp.innerHTML = json.main.temp_min;
-    let minTempContainer = document.querySelector(".minTemp");
-    minTempContainer.appendChild(minTemp);
-    let maxTemp = document.createElement("p");
-    maxTemp.innerHTML = json.main.temp_max;
-    let maxTempContainer = document.querySelector(".maxTemp");
-    maxTempContainer.appendChild(maxTemp);
-  });
-};
+// let displayWeather = () => {
+//   getWeatherAsJSON(
+//     "http://api.openweathermap.org/data/2.5/weather?zip=" +
+//       zipCode +
+//       ",us&units=imperial&appid=7612edd4a0c941f2e5078f8c82d6e4a3"
+//   ).then(json => {
+//     let weatherTemp = document.createElement("p");
+//     weatherTemp.innerHTML = json.main.temp;
+//     let weatherContainer = document.querySelector(".currentTemp");
+//     weatherContainer.appendChild(weatherTemp);
+//     let cityName = document.createElement("p");
+//     cityName.innerHTML = json.name;
+//     let nameContainer = document.querySelector(".cityName");
+//     nameContainer.appendChild(cityName);
+//     let minTemp = document.createElement("p");
+//     minTemp.innerHTML = "Min " + "<br>" + json.main.temp_min;
+//     let minTempContainer = document.querySelector(".minTemp");
+//     minTempContainer.appendChild(minTemp);
+//     let maxTemp = document.createElement("p");
+//     maxTemp.innerHTML = "Max " + "<br>" + json.main.temp_max;
+//     let maxTempContainer = document.querySelector(".maxTemp");
+//     maxTempContainer.appendChild(maxTemp);
+//   });
+// };
 // displayWeather();
 
 let searchButton = document.querySelector(".search");
 
 searchButton.addEventListener("click", function() {
   event.preventDefault();
+  let displayWeather = () => {
+    getWeatherAsJSON(
+      "http://api.openweathermap.org/data/2.5/weather?zip=" +
+        zipCode +
+        ",us&units=imperial&appid=7612edd4a0c941f2e5078f8c82d6e4a3"
+    ).then(json => {
+      let weatherTemp = document.createElement("p");
+      weatherTemp.innerHTML = json.main.temp;
+      let weatherContainer = document.querySelector(".currentTemp");
+      weatherContainer.appendChild(weatherTemp);
+      let cityName = document.createElement("p");
+      cityName.innerHTML = json.name;
+      let nameContainer = document.querySelector(".cityName");
+      nameContainer.appendChild(cityName);
+      let minTemp = document.createElement("p");
+      minTemp.innerHTML = "Min " + "<br>" + json.main.temp_min;
+      let minTempContainer = document.querySelector(".minTemp");
+      minTempContainer.appendChild(minTemp);
+      let maxTemp = document.createElement("p");
+      maxTemp.innerHTML = "Max " + "<br>" + json.main.temp_max;
+      let maxTempContainer = document.querySelector(".maxTemp");
+      maxTempContainer.appendChild(maxTemp);
+    });
+  };
   let zipCode = document.querySelector(".zip").value;
   console.log(zipCode);
   console.log("clicked!");
-  // searchButton.classList.add("hide-me");
+  searchButton.classList.add("hide-me");
   displayWeather();
 });
 
